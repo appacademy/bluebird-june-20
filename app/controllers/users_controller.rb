fa943dd9914a91c599a14d553ae1ebb868e5f484
Jen von Moltke <kennifer09@gmail.com>
2020-07-10 12:04:53 -0700

    Views Lecture W6D5

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,41 +1,64 @@
 class UsersController < ApplicationController
 
   def index
-    users = User.all
-    render json: users
+    @users = User.all
+    # render json: @users
+    render :index
   end
 
   def show
-    user = User.find(params[:id])
-    render json: user
+    # need to use instance variables so they show up in our views!
+    @user = User.find(params[:id])
+    # render json: user
+    render :show
+  end
+
+  def new
+    @user = User.new
+    # make a blank user
+    render :new
   end
 
   def create
-    user = User.new(user_params)
-    if user.save
-      render json: user
+    # debugger
+    @user = User.new(user_params)
+    if @user.save
+      # render json: @user
+      redirect_to user_url(@user)
+      # after user is successfully created, send the client to the show page 
+      # for the user that was just created 
     else
-      render json: user.errors.full_messages, status: 422
+      render json: @user.errors.full_messages, status: 422
     end
   end
 
-  def update
-    user = User.find(params[:id])
+  def edit
+    # debugger
+    @user = User.find(params[:id])
+    # implicitly rendering :edit
+    # because our action is called edit
+    # rails will look for a view called edit.html.erb
+  end
 
-    if user.update(user_params)
-      redirect_to "/users/#{user.id}"
+  def update
+    # debugger
+    @user = User.find(params[:id])
+
+    if @user.update(user_params)
+      redirect_to "/users/#{@user.id}"
     else
-      render json: user.errors.full_messages, status: 422
+      render json: @user.errors.full_messages, status: 422
     end
     
   end
 
   def destroy
-    user = User.find(params[:id])
+    @user = User.find(params[:id])
 
-    user.destroy
+    @user.destroy
 
-    render json: user
+    # render json: @user
+    redirect_to users_url
   end
 
   private
